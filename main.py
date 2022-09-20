9ba1ce60dfdf07b993d0c49bec12cfcc77abbe74
khc1026 <khc1026@cleardentlab.com>
2022-09-21 05:58:10 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+# 샘플 Python 스크립트입니다.
+
+# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
+# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+
+
+def print_hi(name):
+    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
+    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+
+
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+## Ex 5-1. QPushButton.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QPushButton('&Button1', self)
+        btn1.setCheckable(True)
+        btn1.toggle()
+
+        btn2 = QPushButton(self)
+        btn2.setText('Button&2')
+
+        btn3 = QPushButton('Button3', self)
+        btn3.setEnabled(False)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn1)
+        vbox.addWidget(btn2)
+        vbox.addWidget(btn3)
+
+        self.setLayout(vbox)
+        self.setWindowTitle('QPushButton')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
+
+# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
